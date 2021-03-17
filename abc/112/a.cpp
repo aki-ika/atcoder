f949ad7324bf8c2fd9b3e6d068bbdaf0c0232e30
aki-ika <c011821281@edu.teu.ac.jp>
2021-03-17 14:56:57 +0900

    できた

---
+++ abc/112/a.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define rep(i,n) for(ll i=0;i<ll(n);i++)
+#define bit(n) (1LL<<(n))
+#define itn int
+#define vi vector<int>
+
+signed main() {
+  int n;
+  cin >> n;
+  if(n == 2) {
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << endl;
+  }else{
+    cout << "Hello World" << endl;
+  }
+
+}
